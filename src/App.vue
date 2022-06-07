204acabe4943d1c9a07af2cbb9669fc3c7882532
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-07 23:20:03 +0200

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,52 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	font-family: 'montserrat', sans-serif;
+}
+
+#app {
+	background-image: url('./assets/cold-bg.jpeg');
+	background-size: cover;
+	background-position: bottom;
+	transition: 0.4s;
+}
+
+main {
+	min-height: 100vh;
+	padding: 25px;
+
+	background-image: linear-gradient(
+		to bottom,
+		rgba(0, 0, 0, 0.25),
+		rgba(0, 0, 0, 0.75)
+	);
+}
+.search-bar {
+	width: 100%;
+	margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+	display: block;
+	width: 100%;
+	padding: 15px;
+
+	color: #313131;
+	font-size: 20px;
+
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
+
+	background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
